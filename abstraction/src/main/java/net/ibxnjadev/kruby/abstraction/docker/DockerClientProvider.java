f65de012095d4c2b0e3e5e68e1dc227f5494ba2c
Benjamin <supremobenjamin.cia@gmail.com>
2021-06-19 20:53:16 -0400

    added abstraction provider for docker client

--- abstraction/src/main/java/net/ibxnjadev/kruby/abstraction/docker/DockerClientProvider.java
+++ abstraction/src/main/java/net/ibxnjadev/kruby/abstraction/docker/DockerClientProvider.java
@@ -1,7 +1,23 @@
 package net.ibxnjadev.kruby.abstraction.docker;
 
+import com.github.dockerjava.api.DockerClient;
+
+/**
+ * This class provides you with a docker client
+ */
+
 public interface DockerClientProvider {
 
+    /**
+     * Establishes a connection with the docker that is on the machine running the cloud
+     */
 
+    void establishConnection();
+
+    /**
+     * @return get the docker client
+     */
+
+    DockerClient getClient();
 
 }
