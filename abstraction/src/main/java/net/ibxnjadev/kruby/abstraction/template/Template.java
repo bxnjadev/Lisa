c1f2afd8646a48a5d6596843007696b178dfc25c
Benjamin <supremobenjamin.cia@gmail.com>
2021-06-19 23:25:33 -0400

    documented the interface template :rabbit:

--- abstraction/src/main/java/net/ibxnjadev/kruby/abstraction/template/Template.java
+++ abstraction/src/main/java/net/ibxnjadev/kruby/abstraction/template/Template.java
@@ -4,17 +4,51 @@ import net.ibxnjadev.kruby.abstraction.java.JavaVersion;
 import net.ibxnjadev.kruby.abstraction.server.ServerType;
 import net.ibxnjadev.kruby.abstraction.model.Identifiable;
 
+/**
+ * This class represent a template for create server
+ */
+
 public interface Template extends Identifiable {
+
+    /**
+     * The template name
+      * @return template name
+     */
 
     String getName();
 
+    /**
+     * The type of minecraft server used to create servers
+     * @return the type minecraft server
+     */
+
     ServerType getType();
+
+    /**
+     * The name the path jar for run the servers
+     * @return name path jar
+     */
 
     String getPathJar();
 
+    /**
+     * Update the path jar for create servers
+     * @param pathJar the path for run servers
+     */
+
     void updatePathJar(String pathJar);
 
-    int getQuantityServersCreatePerDefect();
+    /**
+     * Obtain the number of servers that will be created from this template when the cloud turns on
+     * @return the number of servers that will be created by default
+     */
+
+    int getQuantityServersCreateByDefect();
+
+    /**
+     * Get the java version the template
+     * @return the java version
+     */
 
     JavaVersion getJavaVersion();
 
