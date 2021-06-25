d2dd554aea7917afda8e2da19b7448bc6d0fd089
Benjamin <supremobenjamin.cia@gmail.com>
2021-06-25 01:39:55 -0400

    added connection in redis :star:

---
+++ core/src/main/java/net/ibxnjadev/kruby/core/redis/CoreRedisClientProvider.java
@@ -0,0 +1,36 @@
+package net.ibxnjadev.kruby.core.redis;
+
+import net.ibxnjadev.kruby.abstraction.redis.RedisClientProvider;
+import net.ibxnjadev.kruby.abstraction.redis.RedisConfiguration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class CoreRedisClientProvider implements RedisClientProvider {
+
+    private final RedisConfiguration configuration;
+    private JedisPool jedisPool;
+    public CoreRedisClientProvider(RedisConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public void establishConnection() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(8);
+
+        JedisPool jedisPool;
+
+        if (configuration.getPassword() != null && !configuration.getPassword().trim().isEmpty()) {
+            jedisPool = new JedisPool(jedisPoolConfig, configuration.getHost(), configuration.getPort(), 2000, configuration.getPassword());
+        } else {
+            jedisPool = new JedisPool(jedisPoolConfig, configuration.getHost(), configuration.getPort(), 2000);
+        }
+
+        this.jedisPool = jedisPool;
+    }
+
+    @Override
+    public JedisPool getClient() {
+        return jedisPool;
+    }
+}
