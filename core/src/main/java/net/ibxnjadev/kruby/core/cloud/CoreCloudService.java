98fc37dfd762a9f1ee72bcd739e5328818661ce1
Benjamin <supremobenjamin.cia@gmail.com>
2021-07-07 01:07:00 -0400

    Updated file util

--- core/src/main/java/net/ibxnjadev/kruby/core/cloud/CoreCloudService.java
+++ core/src/main/java/net/ibxnjadev/kruby/core/cloud/CoreCloudService.java
@@ -1,6 +1,7 @@
 package net.ibxnjadev.kruby.core.cloud;
 
 import com.github.dockerjava.api.DockerClient;
+import net.ibxnjadev.kruby.abstraction.cloud.CloudPortProvider;
 import net.ibxnjadev.kruby.abstraction.cloud.CloudService;
 import net.ibxnjadev.kruby.abstraction.server.Server;
 import net.ibxnjadev.kruby.abstraction.template.Template;
@@ -11,11 +12,12 @@ public class CoreCloudService implements CloudService {
 
     private final Map<String, Server> servers = new HashMap<>();
     private final DockerCloudHandler dockerCloudHandler;
-
     private final DockerClient dockerClient;
+    private final CloudPortProvider cloudPortProvider;
 
-    public CoreCloudService(DockerClient dockerClient) {
+    public CoreCloudService(DockerClient dockerClient, CloudPortProvider cloudPortProvider) {
         this.dockerClient = dockerClient;
+        this.cloudPortProvider = cloudPortProvider;
         dockerCloudHandler = new DockerCloudHandler(dockerClient);
     }
 
--- core/src/main/java/net/ibxnjadev/kruby/core/util/FileUtil.java
+++ core/src/main/java/net/ibxnjadev/kruby/core/util/FileUtil.java
@@ -2,23 +2,33 @@ package net.ibxnjadev.kruby.core.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.function.Consumer;
 
 public class FileUtil {
 
-    public static void createFolderAndExecute(File fileBase, String folderName, Executor executor) {
-        createGenericFileAndExecute(fileBase, folderName, executor, TypeFile.FOLDER);
+
+    public static void createFolderAndExecute(String folderName, Consumer<File> consumer) throws IOException {
+        createFolderAndExecute(null, folderName, consumer);
     }
 
-    public static void createFileAndExecute(File fileBase, String folderName, Executor executor) {
-        createGenericFileAndExecute(fileBase, folderName, executor, TypeFile.NORMAL_FILE);
+    public static void createFileAndExecute(String folderName, Consumer<File> consumer) throws IOException {
+        createFileAndExecute(null, folderName, consumer);
     }
 
-    private static void createGenericFileAndExecute(File fileBase, String folderName, Executor executor, TypeFile typeFile) {
+    public static void createFolderAndExecute(File fileBase, String folderName, Consumer<File> consumer) throws IOException {
+        createGenericFileAndExecute(fileBase, folderName, consumer, TypeFile.FOLDER);
+    }
+
+    public static void createFileAndExecute(File fileBase, String fileName, Consumer<File> consumer) throws IOException {
+        createGenericFileAndExecute(fileBase, fileName, consumer, TypeFile.NORMAL_FILE);
+    }
+
+    private static void createGenericFileAndExecute(File fileBase, String folderName, Consumer<File> consumer, TypeFile typeFile) throws IOException {
         File genericFile = new File(fileBase, folderName);
 
-        if (genericFile.exists()) {
+        if (!genericFile.exists()) {
             createFile(genericFile, typeFile);
-            executor.execute();
+            consumer.accept(genericFile);
         }
     }
 
@@ -34,7 +44,7 @@ public class FileUtil {
         }
     }
 
-    enum TypeFile {
+    static enum TypeFile {
         NORMAL_FILE, FOLDER;
     }
 
