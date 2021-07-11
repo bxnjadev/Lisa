9a41086b482d25bba8f0c9debec812dd18e86ee5
Benjamin <supremobenjamin.cia@gmail.com>
2021-07-11 00:21:48 -0400

    added setters a cloud configuration

--- abstraction/src/main/java/net/ibxnjadev/kruby/abstraction/cloud/CloudConfiguration.java
+++ abstraction/src/main/java/net/ibxnjadev/kruby/abstraction/cloud/CloudConfiguration.java
@@ -5,9 +5,17 @@ import net.ibxnjadev.kruby.abstraction.model.Identifiable;
 
 public interface CloudConfiguration extends Identifiable {
 
+    void setName(String name);
+
+    void setAddress(String address);
+
+    void setMemory(int memory);
+
     String getName();
 
     String getAddress();
+
+    int getMemory();
 
     @JsonProperty("remote_configuration")
     RemoteCloudConfiguration getRemoteCloudConfiguration();
