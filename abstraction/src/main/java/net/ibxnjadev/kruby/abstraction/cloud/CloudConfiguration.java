9dd9b995fd7b9293067c51320cc0ec7d3ee298dd
Benjamin <supremobenjamin.cia@gmail.com>
2021-07-14 12:59:57 -0400

    added route to CloudConfiguration

--- abstraction/src/main/java/net/ibxnjadev/kruby/abstraction/cloud/CloudConfiguration.java
+++ abstraction/src/main/java/net/ibxnjadev/kruby/abstraction/cloud/CloudConfiguration.java
@@ -1,8 +1,12 @@
 package net.ibxnjadev.kruby.abstraction.cloud;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import net.ibxnjadev.kruby.abstraction.annotations.RouteFile;
 import net.ibxnjadev.kruby.abstraction.model.Identifiable;
 
+@RouteFile(
+        route = "configuration.json"
+)
 public interface CloudConfiguration extends Identifiable {
 
     void setId(String id);
