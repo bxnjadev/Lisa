f85602af264acf28827c60ea8f26a377840273a8
Benjamin <supremobenjamin.cia@gmail.com>
2021-08-08 17:16:08 -0400

    added initializer configuration when the cloud is not configured

---
+++ core/src/main/java/net/ibxnjadev/kruby/core/cloud/CoreCloudConfiguration.java
@@ -0,0 +1,59 @@
+package net.ibxnjadev.kruby.core.cloud;
+
+import net.ibxnjadev.kruby.abstraction.cloud.CloudConfiguration;
+import net.ibxnjadev.kruby.abstraction.cloud.RemoteCloudConfiguration;
+
+public class CoreCloudConfiguration implements CloudConfiguration {
+
+    private String id;
+    private String name;
+    private String address;
+    private int memory;
+    private RemoteCloudConfiguration remoteCloudConfiguration;
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public int getMemory() {
+        return memory;
+    }
+
+    @Override
+    public RemoteCloudConfiguration getRemoteCloudConfiguration() {
+        return remoteCloudConfiguration;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+}
--- core/src/main/java/net/ibxnjadev/kruby/core/setup/DefaultCloudSetupService.java
+++ core/src/main/java/net/ibxnjadev/kruby/core/setup/DefaultCloudSetupService.java
@@ -4,21 +4,24 @@ import net.ibxnjadev.kruby.abstraction.cloud.CloudConfiguration;
 import net.ibxnjadev.kruby.abstraction.redis.RedisClientProvider;
 import net.ibxnjadev.kruby.abstraction.redis.RedisConfiguration;
 import net.ibxnjadev.kruby.abstraction.setup.CloudSetupService;
+import net.ibxnjadev.kruby.core.cloud.CoreCloudConfiguration;
 import net.ibxnjadev.kruby.core.redis.CoreRedisClientProvider;
 import net.ibxnjadev.kruby.core.util.ObjectFileStorageHelper;
 
 public class DefaultCloudSetupService implements CloudSetupService {
 
-    private CloudConfiguration configuration = null;
-
     @Override
     public void setup() {
+
+        CloudConfiguration configuration = null;
 
         if (!cloudIsConfigured()) {
             System.out.println("");
             System.out.println("Welcome to Kruby (Minecraft Cloud)");
             System.out.println("Setup cloud");
             System.out.println("");
+
+            configuration = new CoreCloudConfiguration();
 
             setups(
                     new SetupDirectory(),
