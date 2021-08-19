630fe59b1f52a87162b5532d96bc71ada7dda384
Benjamin <supremobenjamin.cia@gmail.com>
2021-08-19 19:13:52 -0400

    annotation added in list template command

--- core/src/main/java/net/ibxnjadev/kruby/core/command/template/ListTemplateCommand.java
+++ core/src/main/java/net/ibxnjadev/kruby/core/command/template/ListTemplateCommand.java
@@ -1,8 +1,10 @@
 package net.ibxnjadev.kruby.core.command.template;
 
 import me.fixeddev.commandflow.annotated.CommandClass;
+import me.fixeddev.commandflow.annotated.annotation.Command;
 import net.ibxnjadev.kruby.core.template.TemplateService;
 
+@Command(names = "templatelist")
 public class ListTemplateCommand implements CommandClass {
 
     private final TemplateService templateService;
@@ -11,6 +13,7 @@ public class ListTemplateCommand implements CommandClass {
         this.templateService = templateService;
     }
 
+    @Command(names = "")
     public void main() {
 
         System.out.println("[Kruby] Templates: ");
