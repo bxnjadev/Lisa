7367df5ce835dfa3da67a607645287f3d7ea35db
Benjamin <supremobenjamin.cia@gmail.com>
2021-08-20 17:27:39 -0400

    Documented RouteFile

--- core/src/main/java/net/ibxnjadev/kruby/core/annotations/RouteFile.java
+++ core/src/main/java/net/ibxnjadev/kruby/core/annotations/RouteFile.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Used for can convert a object to file
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(value = ElementType.TYPE)
 public @interface RouteFile {
